799e9122d969de768ef6e3f635d96cd9e71d8baa
Javier Duarte <javiduarte34@gmail.com>
2024-10-01 14:33:31 +0200

    Primer commit

---
+++ src/test/java/com/mrrebujito/mrrebujito/MrrebujitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mrrebujito.mrrebujito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MrrebujitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
